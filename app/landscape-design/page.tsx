62d5d77f311c15512ff77ace3c930e21d5c88697
LordLingo <mark@precisehire.com>
2026-07-25 17:46:26 -0500

    Build landscape design SEO page

---
+++ app/landscape-design/page.tsx
@@ -0,0 +1,17 @@
+import { buildServiceMetadata, serviceList } from "../service-data";
+import ServicePage from "../service-page";
+import { landscapeDesignPage } from "../seo-page-data";
+
+export const metadata = buildServiceMetadata(landscapeDesignPage);
+
+export default function LandscapeDesignPage() {
+  return (
+    <ServicePage
+      service={landscapeDesignPage}
+      relatedServices={serviceList}
+      faqTitle="Questions homeowners ask before a landscape design project."
+      relatedEyebrow="Designed as one landscape"
+      relatedTitle="Services that can be planned together."
+    />
+  );
+}
