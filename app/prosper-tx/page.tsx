a390c4cf6af9d3806db394118b7f93b0d89a93b8
LordLingo <mark@precisehire.com>
2026-07-25 17:48:53 -0500

    Balance Prosper related service links

--- app/prosper-tx/page.tsx
+++ app/prosper-tx/page.tsx
@@ -1,10 +1,17 @@
-import { buildServiceMetadata, serviceList } from "../service-data";
+import { buildServiceMetadata, services } from "../service-data";
 import ServicePage from "../service-page";
 import { landscapeDesignPage, prosperPage } from "../seo-page-data";
 
 export const metadata = buildServiceMetadata(prosperPage);
 
-const prosperRelatedPages = [landscapeDesignPage, ...serviceList];
+const prosperRelatedPages = [
+  landscapeDesignPage,
+  services.frontYard,
+  services.flowerBeds,
+  services.drainage,
+  services.stone,
+  services.lighting,
+];
 
 export default function ProsperLandscapingPage() {
   return (
